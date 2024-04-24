d75caa6bacdbbb909a07421a240969b11e3c2ec6
Gareth Wu <86927756+garywuuu@users.noreply.github.com>
2024-04-23 22:14:05 -0400

    Add files via upload

---
+++ simulation.py
@@ -0,0 +1,14 @@
+import numpy as np 
+
+class Agent:
+  def __init__(self):
+    self.id = id
+    self.age = age
+
+  def print_details(self):
+    print(f"Name: {self.name}")
+    print(f"Age: {self.age}")
+
+class Learner:
+   def __init__(self):
+     self.paramaters = [0.5,0.5]
